c78cc157046f90b747b331eda9af9bd6ad50354e
dr-gavrikov <dr_gavrikov@outlook.com>
2022-02-13 15:47:47 +0300

    problem-690

---
+++ src/main/kotlin/Problem690.kt
@@ -0,0 +1,26 @@
+/**
+ * @author Aleksandr Gavrikov
+ * @url https://leetcode.com/problems/employee-importance/
+ */
+class Problem690 {
+
+    class Employee(id: Int, importance: Int, subordinates: List<Int>) {
+        var id = id
+        var importance = importance
+        var subordinates: List<Int> = subordinates
+    }
+
+    fun getImportance(employees: List<Employee?>, id: Int): Int {
+        val used = mutableSetOf<Employee>()
+        fun dfs(employee: Employee?) {
+            if (employee == null) return
+            if (employee in used) return
+            used.add(employee)
+            employee.subordinates.forEach { index ->
+                dfs(employees.firstOrNull { it?.id == index })
+            }
+        }
+        dfs(employees.firstOrNull { it?.id == id })
+        return used.map { it.importance }.sum()
+    }
+}
---
+++ src/test/kotlin/Problem690Test.kt
@@ -0,0 +1,19 @@
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DynamicTest
+
+
+class Problem690Test : AbstractTest() {
+
+    override fun dynamicTests(): Collection<DynamicTest> {
+        val problem690 = Problem690()
+        return listOf(
+            DynamicTest.dynamicTest("test1") {
+                val employee1 = Problem690.Employee(1, 5, listOf(2, 3))
+                val employee2 = Problem690.Employee(2, 3, listOf())
+                val employee3 = Problem690.Employee(3, 3, listOf())
+                Assertions.assertThat(problem690.getImportance(listOf(employee1, employee2, employee3), 1))
+                    .isEqualTo(11)
+            }
+        )
+    }
+}
