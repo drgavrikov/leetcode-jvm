86408d446101269e06b0bf73e3a8ec19ebe61ffa
dr-gavrikov <dr_gavrikov@outlook.com>
2023-09-10 17:45:52 +0400

    problem-54

--- src/main/kotlin/medium/Problem54.kt
+++ src/main/kotlin/medium/Problem54.kt
@@ -15,24 +15,24 @@ class Problem54 {
         var top = 0
         var bottom = n - 1
         var left = 0
-        var rigth = m - 1
+        var right = m - 1
 
         while (true) {
-            for (col in left..rigth) result.add(matrix[top][col])
+            for (col in left..right) result.add(matrix[top][col])
             top++
             if (top > bottom) break
 
-            for (row in top..bottom) result.add(matrix[row][rigth])
-            rigth--
-            if (left > rigth) break
+            for (row in top..bottom) result.add(matrix[row][right])
+            right--
+            if (left > right) break
 
-            for (col in rigth downTo left) result.add(matrix[bottom][col])
+            for (col in right downTo left) result.add(matrix[bottom][col])
             bottom--
             if (top > bottom) break
 
             for (row in bottom downTo top) result.add(matrix[row][left])
             left++
-            if (left > rigth) break
+            if (left > right) break
         }
         return result
     }
