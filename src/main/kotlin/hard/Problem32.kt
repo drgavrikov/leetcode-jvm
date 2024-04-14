57ef99a5cd07b04100c5e232c1dce5584e255f45
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-15 01:09:05 +0400

    problem-32

---
+++ src/main/kotlin/hard/Problem32.kt
@@ -0,0 +1,45 @@
+package hard
+
+import kotlin.math.max
+
+/**
+ * @author Aleksandr Gavrikov
+ * @url https://leetcode.com/problems/longest-valid-parentheses/solutions/5024031/linear-solution-without-additional-memory/
+ */
+class Problem32 {
+
+    fun longestValidParentheses(s: String): Int {
+        var result = 0
+
+        var openedCount = 0
+        var closedCount = 0
+
+        var reversedOpenedCount = 0
+        var reversedClosedCount = 0
+
+        s.indices.forEach { index ->
+            if (s[index] == '(') openedCount++
+            else closedCount++
+            if (openedCount == closedCount) result = max(result, openedCount * 2)
+            if (closedCount > openedCount) {
+                openedCount = 0
+                closedCount = 0
+            }
+
+            if (s[s.length - index - 1] == '(') reversedOpenedCount++
+            else reversedClosedCount++
+            if (reversedOpenedCount == reversedClosedCount) result = max(result, reversedClosedCount * 2)
+            if (reversedOpenedCount > reversedClosedCount) {
+                reversedOpenedCount = 0
+                reversedClosedCount = 0
+            }
+        }
+        return result
+    }
+}
+
+fun main() {
+    val problem32 = Problem32()
+    check(4 == problem32.longestValidParentheses("((()()"))
+    check(4 == problem32.longestValidParentheses("))()()"))
+}
