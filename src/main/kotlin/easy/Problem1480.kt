dcc58e675ca85984d9cba32fd4098005f4d63032
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 02:10:09 +0400

    problem-1480

--- src/main/kotlin/easy/Problem1480.kt
+++ src/main/kotlin/easy/Problem1480.kt
@@ -15,3 +15,22 @@ class Problem1480 {
         return result
     }
 }
+
+fun main() {
+    val problem1480 = Problem1480()
+
+    runDynamicTest("test1") {
+        val actual = problem1480.runningSum(intArrayOf(1, 2, 3, 4))
+        check(actual.contentEquals(intArrayOf(1, 3, 6, 10))) { "Test failed: Expected [1, 3, 6, 10], actual: ${actual.contentToString()}" }
+    }
+
+    runDynamicTest("test2") {
+        val actual = problem1480.runningSum(intArrayOf(1, 1, 1, 1, 1))
+        check(actual.contentEquals(intArrayOf(1, 2, 3, 4, 5))) { "Test failed: Expected [1, 2, 3, 4, 5], actual: ${actual.contentToString()}" }
+    }
+
+    runDynamicTest("test3") {
+        val actual = problem1480.runningSum(intArrayOf(1, 2, 3))
+        check(actual.contentEquals(intArrayOf(1, 3, 6))) { "Test failed: Expected [1, 3, 6], actual: ${actual.contentToString()}" }
+    }
+}
