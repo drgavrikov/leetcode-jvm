e0dc47c74b6cc47af3ebd105e8390a1943b61253
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 02:00:29 +0400

    problem-326

--- src/main/kotlin/easy/Problem326.kt
+++ src/main/kotlin/easy/Problem326.kt
@@ -9,6 +9,30 @@ class Problem326 {
         if (n <= 0) return false
         var x = n
         while (x % 3 == 0) x /= 3
-        return x == 1 || x == -1
+        return x == 1
+    }
+}
+
+fun main() {
+    val problem326 = Problem326()
+
+    runDynamicTest("test1") {
+        val actual = problem326.isPowerOfThree(3)
+        check(actual) { "Test failed: Expected true, actual: $actual" }
+    }
+
+    runDynamicTest("test2") {
+        val actual = problem326.isPowerOfThree(-27)
+        check(!actual) { "Test failed: Expected false, actual: $actual" }
+    }
+
+    runDynamicTest("test3") {
+        val actual = problem326.isPowerOfThree(15)
+        check(!actual) { "Test failed: Expected false, actual: $actual" }
+    }
+
+    runDynamicTest("test4") {
+        val actual = problem326.isPowerOfThree(0)
+        check(!actual) { "Test failed: Expected false, actual: $actual" }
     }
 }
