e36613880038d588b342266f248ac620a4030236
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 01:53:33 +0400

    problem-100

--- src/main/kotlin/easy/Problem100.kt
+++ src/main/kotlin/easy/Problem100.kt
@@ -17,3 +17,31 @@ class Problem100 {
         return dfs(p, q)
     }
 }
+
+fun main() {
+    val problem100 = Problem100()
+
+    runDynamicTest("test1") {
+        val p = TreeNode(1)
+        p.left = TreeNode(2)
+        p.right = TreeNode(3)
+
+        val q = TreeNode(1)
+        q.left = TreeNode(2)
+        q.right = TreeNode(3)
+
+        val actual = problem100.isSameTree(p, q)
+        check(actual) { "Test failed: Expected true, actual: $actual" }
+    }
+
+    runDynamicTest("test2") {
+        val p = TreeNode(1)
+        p.left = TreeNode(2)
+
+        val q = TreeNode(1)
+        q.right = TreeNode(2)
+
+        val actual = problem100.isSameTree(p, q)
+        check(!actual) { "Test failed: Expected false, actual: $actual" }
+    }
+}
