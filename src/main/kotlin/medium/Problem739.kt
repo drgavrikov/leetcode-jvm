8a24e62cef8626bb84529d3be48193794e61c335
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 00:10:52 +0400

    problem-739

--- src/main/kotlin/medium/Problem739.kt
+++ src/main/kotlin/medium/Problem739.kt
@@ -1,22 +1,31 @@
 package medium
 
-import java.util.*
-
 /**
  * @author Aleksandr Gavrikov
- * @url https://leetcode.com/problems/daily-temperatures/
+ * @url https://leetcode.com/problems/daily-temperatures/solutions/5053738/iterating-backwards-solution-without-stack-and-additional-memory/
  */
 class Problem739 {
     fun dailyTemperatures(temperatures: IntArray): IntArray {
-        val indices = Stack<Int>()
-        val result = IntArray(temperatures.size) { 0 }
-        temperatures.indices.forEach { index ->
-            while (indices.isNotEmpty() && temperatures[index] > temperatures[indices.last()]) {
-                val last = indices.pop()!!
-                result[last] = index - last
+        val n = temperatures.size
+        val result = IntArray(n) {0}
+
+        var hottest = 0
+        for (index in (n - 1) downTo 0) {
+            val temperature = temperatures[index]
+            if (temperature >= hottest) {
+                hottest = temperature
+                continue
             }
-            indices.add(index)
+            var days = 1
+            while (temperatures[index + days] <= temperature) days += result[index + days]
+            result[index] = days
         }
         return result
     }
+}
+
+fun main() {
+    val problem = Problem739()
+    val actual = problem.dailyTemperatures(intArrayOf(73, 74, 75, 71, 69, 72, 76, 73))
+    check(actual.contentEquals(intArrayOf(1, 1, 4, 2, 1, 1, 0, 0)))
 }
