74c519727f358b51c11022ab099eb58880e9567d
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 02:28:32 +0400

    problem-987

--- src/main/kotlin/hard/Problem987.kt
+++ src/main/kotlin/hard/Problem987.kt
@@ -29,3 +29,39 @@ class Problem987 {
             }.toList()
     }
 }
+
+fun main() {
+    val problem987 = Problem987()
+
+    runDynamicTest("test1") {
+        val root = TreeNode(3)
+
+        root.left = TreeNode(9)
+        root.right = TreeNode(20)
+
+        root.right!!.left = TreeNode(15)
+        root.right!!.right = TreeNode(7)
+
+        val actual = problem987.verticalTraversal(root)
+        check(
+            actual == listOf(listOf(9), listOf(3, 15), listOf(20), listOf(7))
+        ) { "Test failed: Expected [[9], [3, 15], [20], [7]], actual: $actual" }
+    }
+
+    runDynamicTest("test2") {
+        val root = TreeNode(1)
+
+        root.left = TreeNode(2)
+        root.right = TreeNode(3)
+
+        root.left!!.left = TreeNode(4)
+        root.left!!.right = TreeNode(5)
+        root.right!!.left = TreeNode(6)
+        root.right!!.right = TreeNode(7)
+
+        val actual = problem987.verticalTraversal(root)
+        check(
+            actual == listOf(listOf(4), listOf(2), listOf(1, 5, 6), listOf(3), listOf(7))
+        ) { "Test failed: Expected [[4], [2], [1, 5, 6], [3], [7]], actual: $actual" }
+    }
+}
