18aea7cca630b98edd0f439cc5bb501531e5ac16
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 02:10:49 +0400

    problem-1672

--- src/main/kotlin/easy/Problem1672.kt
+++ src/main/kotlin/easy/Problem1672.kt
@@ -12,3 +12,14 @@ class Problem1672 {
         return accounts.maxOf { array -> array.sum() }
     }
 }
+
+fun main() {
+    val problem1672 = Problem1672()
+
+    runDynamicTest("test1") {
+        val actual = problem1672.maximumWealth(
+            arrayOf(intArrayOf(1, 2, 3), intArrayOf(3, 2, 1))
+        )
+        check(actual == 6) { "Test failed: Expected 6, actual: $actual" }
+    }
+}
