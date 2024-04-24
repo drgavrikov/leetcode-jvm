d786b54bbe449369db4a31281fb95ac61df4f02f
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 01:59:04 +0400

    problem-141

--- src/main/kotlin/easy/Problem141.kt
+++ src/main/kotlin/easy/Problem141.kt
@@ -19,3 +19,40 @@ class Problem141 {
         return false
     }
 }
+
+fun main() {
+    val problem = Problem141()
+
+    runDynamicTest("test1") {
+        val node3 = ListNode(3)
+        val node2 = ListNode(2)
+        val node0 = ListNode(0)
+        val node4 = ListNode(4)
+
+        node3.next = node2
+        node2.next = node0
+        node0.next = node4
+        node4.next = node2
+
+        check(problem.hasCycle(node3)) { "Test failed: Expected true, actual: false" }
+    }
+
+    runDynamicTest("test2") {
+        val node1 = ListNode(2)
+        val node2 = ListNode(2)
+
+        node1.next = node2
+        node2.next = node1
+
+        check(problem.hasCycle(node1)) { "Test failed: Expected true, actual: false" }
+    }
+
+    runDynamicTest("test3") {
+        val node1 = ListNode(2)
+        val node2 = ListNode(2)
+
+        node1.next = node2
+
+        check(!problem.hasCycle(node1)) { "Test failed: Expected false, actual: true" }
+    }
+}
