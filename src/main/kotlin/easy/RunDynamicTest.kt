8b66c50ad7fcb65eaa4bd0734bc93c71d34eb0a5
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 01:50:45 +0400

    problem-26

---
+++ src/main/kotlin/easy/RunDynamicTest.kt
@@ -0,0 +1,10 @@
+package easy
+
+fun runDynamicTest(name: String, test: () -> Unit) {
+    try {
+        test()
+        println("Test $name passed")
+    } catch (e: AssertionError) {
+        println("Test $name failed: ${e.message}")
+    }
+}
