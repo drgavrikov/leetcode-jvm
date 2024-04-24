7d6db99e7986fb767c96967b104bd1970de82ae5
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 01:56:57 +0400

    problem-136

--- src/main/kotlin/easy/Problem136.kt
+++ src/main/kotlin/easy/Problem136.kt
@@ -14,3 +14,22 @@ class Problem136 {
         return numSet.first()
     }
 }
+
+fun main() {
+    val problem136 = Problem136()
+
+    runDynamicTest("test1") {
+        val actual = problem136.singleNumber(intArrayOf(2, 2, 1))
+        check(actual == 1) { "Test failed: Expected 1, actual: $actual" }
+    }
+
+    runDynamicTest("test2") {
+        val actual = problem136.singleNumber(intArrayOf(4, 1, 2, 1, 2))
+        check(actual == 4) { "Test failed: Expected 4, actual: $actual" }
+    }
+
+    runDynamicTest("test3") {
+        val actual = problem136.singleNumber(intArrayOf(1))
+        check(actual == 1) { "Test failed: Expected 1, actual: $actual" }
+    }
+}
