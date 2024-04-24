6c299624b6e37010ea382f6850b30f81c528c9d3
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 02:08:17 +0400

    problem-942

--- src/main/kotlin/easy/Problem942.kt
+++ src/main/kotlin/easy/Problem942.kt
@@ -12,3 +12,17 @@ class Problem942 {
         return (s.map { char -> if (char == 'I') i++ else d-- } + i).toIntArray()
     }
 }
+
+fun main() {
+    val problem = Problem942()
+
+    runDynamicTest("test1") {
+        val actual = problem.diStringMatch("IDID")
+        check(actual.contentEquals(intArrayOf(0, 4, 1, 3, 2))) { "Test failed: Expected [0, 4, 1, 3, 2], actual: ${actual.contentToString()}" }
+    }
+
+    runDynamicTest("test2") {
+        val actual = problem.diStringMatch("III")
+        check(actual.contentEquals(intArrayOf(0, 1, 2, 3))) { "Test failed: Expected [0, 1, 2, 3], actual: ${actual.contentToString()}" }
+    }
+}
