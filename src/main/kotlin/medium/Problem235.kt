fa894f364f20b9958b89a81056643009fd9aec18
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 00:56:15 +0400

    problem-235

---
+++ src/main/kotlin/medium/Problem235.kt
@@ -0,0 +1,31 @@
+package medium
+
+import TreeNode
+
+/**
+ * @author Aleksandr Gavrikov
+ * @url https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ */
+class Problem235 {
+
+    fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+        var node = root
+        checkNotNull(p)
+        checkNotNull(q)
+        while (node != null) {
+            if (p.`val` < node.`val` && q.`val` < node.`val`) node = node.left
+            else if (node.`val` < p.`val` && q.`val` < node.`val`) node = node.right
+            else return node
+        }
+        return null
+    }
+}
+
+fun main() {
+    val problem = Problem235()
+    val node = TreeNode(1)
+    val root = TreeNode(2)
+    root.left = node
+    val actual = problem.lowestCommonAncestor(root, root, node)
+    check(actual == root)
+}
