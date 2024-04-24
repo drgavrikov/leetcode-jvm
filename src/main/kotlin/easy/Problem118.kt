66c6ac2a02351b87861c56c1d2ebb19e29147057
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 01:54:42 +0400

    problem-118

--- src/main/kotlin/easy/Problem118.kt
+++ src/main/kotlin/easy/Problem118.kt
@@ -14,3 +14,19 @@ class Problem118 {
         return triangle
     }
 }
+
+fun main() {
+    val problem118 = Problem118()
+
+    runDynamicTest("test1") {
+        val actual = problem118.generate(5)
+        val triangle = listOf(
+            listOf(1),
+            listOf(1, 1),
+            listOf(1, 2, 1),
+            listOf(1, 3, 3, 1),
+            listOf(1, 4, 6, 4, 1)
+        )
+        check(actual == triangle) { "Test failed: Expected $triangle, actual: $actual" }
+    }
+}
