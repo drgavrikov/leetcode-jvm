b8994e45b73d1806fb58d9ddaa6e19ca7145f5f0
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 01:03:16 +0400

    problem-356

---
+++ src/main/kotlin/medium/Problem356.kt
@@ -0,0 +1,29 @@
+package medium
+
+/**
+ * @author Aleksandr Gavrikov
+ * @url https://leetcode.com/problems/design-hit-counter/description/
+ */
+class Problem356 {
+    fun isReflected(points: Array<IntArray>): Boolean {
+        val coordinates = points
+            .groupBy({ it[0] }, { it[1] })
+            .mapValues { (_, ys) -> ys.toMutableSet() }
+
+        val min = coordinates.keys.minOrNull()!!
+        val max = coordinates.keys.maxOrNull()!!
+        val middle = (min + max * 1.0) / 2
+
+        return points.all { (x, y) ->
+            val reflectedX = (middle - (x - middle)).toInt()
+            reflectedX in coordinates && y in coordinates.getValue(reflectedX)
+        }
+    }
+}
+
+fun main() {
+    val problem = Problem356()
+    check(problem.isReflected(arrayOf(intArrayOf(1, 1), intArrayOf(-1, 1))))
+    check(!problem.isReflected(arrayOf(intArrayOf(1, 1), intArrayOf(-1, -1))))
+    check(problem.isReflected(arrayOf(intArrayOf(-5, 1), intArrayOf(5, 1), intArrayOf(5, 1))))
+}
