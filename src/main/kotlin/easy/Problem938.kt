789d07ff34d58ca10938861239bf6cf8fa511337
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 02:07:32 +0400

    problem-938

--- src/main/kotlin/easy/Problem938.kt
+++ src/main/kotlin/easy/Problem938.kt
@@ -19,4 +19,23 @@ class Problem938 {
     }
 }
 
+fun main() {
+    val problem = Problem938()
 
+    runDynamicTest("test1") {
+        val node3 = TreeNode(3)
+        val node5 = TreeNode(5)
+        val node18 = TreeNode(18)
+        val node7 = TreeNode(7)
+        node5.left = node3
+        node5.right = node7
+        val node15 = TreeNode(15)
+        node15.right = node18
+        val node10 = TreeNode(10)
+        node10.left = node5
+        node10.right = node15
+
+        val actual = problem.rangeSumBST(node10, 7, 15)
+        check(actual == 32) { "Test failed: Expected 32, actual: $actual" }
+    }
+}
