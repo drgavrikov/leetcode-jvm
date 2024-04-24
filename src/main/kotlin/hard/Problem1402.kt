52f70a5fe48605110b646f786ffc187477f51b0f
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 02:25:44 +0400

    problem-1402

--- src/main/kotlin/hard/Problem1402.kt
+++ src/main/kotlin/hard/Problem1402.kt
@@ -19,3 +19,17 @@ class Problem1402 {
         return sum
     }
 }
+
+fun main() {
+    val problem1402 = Problem1402()
+
+    runDynamicTest("test1") {
+        val actual = problem1402.maxSatisfaction(intArrayOf(-1, -8, 0, 5, -9))
+        check(actual == 14) { "Test failed: Expected 14, actual: $actual" }
+    }
+
+    runDynamicTest("test2") {
+        val actual = problem1402.maxSatisfaction(intArrayOf(4, 3, 2))
+        check(actual == 20) { "Test failed: Expected 20, actual: $actual" }
+    }
+}
