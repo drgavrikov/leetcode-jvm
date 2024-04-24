bb4445b9d4ce699a2943649793e62c941f7faac7
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 01:52:38 +0400

    problem-88

--- src/main/kotlin/easy/Problem88.kt
+++ src/main/kotlin/easy/Problem88.kt
@@ -1,13 +1,11 @@
 package easy
 
-import TreeNode
-
 /**
  * @author Aleksandr Gavrikov
  * @url https://leetcode.com/problems/merge-sorted-array/description/
  */
 class Problem88 {
-    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
+    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int) {
         var index1 = m - 1
         var index2 = n - 1
         var index = n + m - 1
@@ -19,5 +17,18 @@ class Problem88 {
             }
         }
         while (index2 >= 0) nums1[index--] = nums2[index2--]
+    }
+}
+
+fun main() {
+    val problem = Problem88()
+
+    runDynamicTest("test1") {
+        val num1 = intArrayOf(1, 2, 3, 0, 0, 0)
+        val n = 3
+        val num2 = intArrayOf(2, 5, 6)
+        val m = 3
+        problem.merge(num1, n, num2, m)
+        check(num1.contentEquals(intArrayOf(1, 2, 2, 3, 5, 6))) { "Test failed: Expected [1, 2, 2, 3, 5, 6], actual: ${num1.contentToString()}" }
     }
 }
