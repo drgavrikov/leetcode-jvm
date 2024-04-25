4b05b029940b04fe8d154b8e33599b1c618c9a0b
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 11:16:16 +0400

    problem-222

--- src/main/kotlin/medium/Problem222.kt
+++ src/main/kotlin/medium/Problem222.kt
@@ -20,3 +20,26 @@ class Problem222 {
         return count
     }
 }
+
+fun main() {
+    val problem222 = Problem222()
+
+    val node1 = TreeNode(1)
+    val node2 = TreeNode(2)
+    val node3 = TreeNode(3)
+    val node4 = TreeNode(4)
+    val node5 = TreeNode(3)
+    val node6 = TreeNode(4)
+
+    node1.left = node2
+    node1.right = node3
+
+    node2.left = node4
+    node2.right = node5
+
+    node3.left = node6
+
+    runDynamicTest("test1") {
+        check(problem222.countNodes(node1) == 6) { "Test 1 failed: Expected result to be 6" }
+    }
+}
