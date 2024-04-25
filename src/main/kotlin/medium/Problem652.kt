5cdb910b1c2c5c0e45cb3d8bd158ee8dd1908742
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 11:25:42 +0400

    problem-652

--- src/main/kotlin/medium/Problem652.kt
+++ src/main/kotlin/medium/Problem652.kt
@@ -25,3 +25,22 @@ class Problem652 {
         return result
     }
 }
+
+fun main() {
+    val problem652 = Problem652()
+
+    val node1 = TreeNode(1)
+    val node2 = TreeNode(2)
+    val node3 = TreeNode(2)
+    val node4 = TreeNode(3)
+    val node5 = TreeNode(3)
+
+    node1.left = node2
+    node1.right = node3
+
+    node2.left = node4
+    node3.left = node5
+
+    val actual = problem652.findDuplicateSubtrees(node1)
+    check(actual.toSet() == setOf(node3, node5)) { "Test 1 failed" }
+}
