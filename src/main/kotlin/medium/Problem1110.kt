13007626e6e6ca0757842c537618e96872b83e43
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 11:37:03 +0400

    problem-1110

--- src/main/kotlin/medium/Problem1110.kt
+++ src/main/kotlin/medium/Problem1110.kt
@@ -38,3 +38,26 @@ class Problem1110 {
         return result
     }
 }
+
+fun main() {
+    val problem1110 = Problem1110()
+
+    val node1 = TreeNode(1)
+    val node2 = TreeNode(2)
+    val node3 = TreeNode(3)
+    val node4 = TreeNode(4)
+    val node5 = TreeNode(5)
+    val node6 = TreeNode(6)
+    val node7 = TreeNode(7)
+
+    node1.left = node2
+    node1.right = node3
+    node2.left = node4
+    node2.right = node5
+    node3.left = node6
+    node3.right = node7
+
+    val actual = problem1110.delNodes(node1, intArrayOf(3, 4))
+    check(actual == listOf(node1, node6, node7)) { "Test 1 failed" }
+}
+
