fe99117a890b0b7437f535bc6945931d4b2394a5
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 11:09:30 +0400

    problem-114

--- src/main/kotlin/medium/Problem114.kt
+++ src/main/kotlin/medium/Problem114.kt
@@ -7,7 +7,7 @@ import TreeNode
  * @url https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
  */
 class Problem114 {
-    fun flatten(root: TreeNode?): Unit {
+    fun flatten(root: TreeNode?) {
         val preOrderNodes = mutableListOf<TreeNode>()
 
         fun dfs(root: TreeNode?) {
@@ -25,5 +25,34 @@ class Problem114 {
         preOrderNodes.zipWithNext().forEach { (prev, next) ->
             prev.right = next
         }
+    }
+}
+
+fun main() {
+    val problem = Problem114()
+
+    val node1 = TreeNode(1)
+    val node2 = TreeNode(2)
+    val node3 = TreeNode(3)
+    val node4 = TreeNode(4)
+    val node5 = TreeNode(5)
+    val node6 = TreeNode(6)
+
+    node2.left = node3
+    node2.right = node4
+    node5.right = node6
+    node1.left = node2
+    node1.right = node5
+
+    runDynamicTest("test1") {
+        problem.flatten(node1)
+        val expected = listOf(1, 2, 3, 4, 5, 6)
+        val actual = mutableListOf<Int>()
+        var current: TreeNode? = node1
+        while (current != null) {
+            actual.add(current.`val`)
+            current = current.right
+        }
+        check(actual == expected) { "Test 1 failed: Expected $expected, but got $actual" }
     }
 }
