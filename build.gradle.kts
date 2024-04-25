27eaf15e47142223385e88f9f5b60d89936012b1
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 11:48:05 +0400

    delete extra Test

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,11 +18,6 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-    testImplementation("org.assertj:assertj-core:3.11.1")
-    testImplementation("org.junit.platform:junit-platform-runner:1.7.0")
 }
 
 tasks.withType<KotlinCompile> {
