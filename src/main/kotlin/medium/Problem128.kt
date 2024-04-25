38b106766a68e302004cf7f79b032d956ab9b7b0
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-25 11:10:26 +0400

    problem-128

--- src/main/kotlin/medium/Problem128.kt
+++ src/main/kotlin/medium/Problem128.kt
@@ -18,3 +18,17 @@ class Problem128 {
         return result
     }
 }
+
+fun main() {
+    val problem = Problem128()
+
+    runDynamicTest("test1") {
+        val actual = problem.longestConsecutive(intArrayOf(0, 3, 7, 2, 5, 8, 4, 6, 0, 1))
+        check(actual == 9) { "Test 1 failed: Expected 9, but got $actual" }
+    }
+
+    runDynamicTest("test2") {
+        val actual = problem.longestConsecutive(intArrayOf(100, 4, 200, 1, 3, 2))
+        check(actual == 4) { "Test 2 failed: Expected 4, but got $actual" }
+    }
+}
