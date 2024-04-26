cb24a703919847dd80e8c396d402ccfdb8b0758a
dr_gavrikov <agavrikov1989@gmail.com>
2024-04-26 13:20:35 +0400

    problem-2370

---
+++ src/main/kotlin/Problem2370.kt
@@ -0,0 +1,32 @@
+import kotlin.math.abs
+import kotlin.math.max
+
+/**
+ * @author Aleksandr Gavrikov
+ * @url https://leetcode.com/problems/longest-ideal-subsequence/
+ */
+class Problem2370 {
+    fun longestIdealString(s: String, k: Int): Int {
+        val dp = IntArray(26) { 0 }
+        s.forEach { char ->
+            val index = char - 'a'
+            var result = 0
+            for (prev in 0 until 26) {
+                if (abs(prev - index) <= k)
+                    result = max(result, dp[prev])
+            }
+            dp[index] = result + 1
+        }
+        return dp.maxOf { it }
+    }
+}
+
+fun main() {
+    val problem = Problem2370()
+
+    val actual1 = problem.longestIdealString("acfgbd", 2)
+    check(actual1 == 4) { "Test failed: Expected 4, actual: $actual1" }
+
+    val actual2 = problem.longestIdealString("abcd", 3)
+    check(actual1 == 4) { "Test failed: Expected 4, actual: $actual2    " }
+}
