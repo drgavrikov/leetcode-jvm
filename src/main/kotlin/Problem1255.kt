8748cffcd5721382ca1d2a2204deae512efb91c1
dr_gavrikov <agavrikov1989@gmail.com>
2024-05-25 16:48:19 +0400

    problem-1225

--- src/main/kotlin/Problem1255.kt
+++ src/main/kotlin/Problem1255.kt
@@ -1,35 +1,24 @@
-import kotlin.math.max
-
 /**
  * @author Aleksandr Gavrikov
- * @url https://leetcode.com/problems/maximum-score-words-formed-by-letters/
+ * @url https://leetcode.com/problems/maximum-score-words-formed-by-letters/solutions/5205505/brute-force-iterative-loop-for-every-subset/
  */
 class Problem1255 {
     fun maxScoreWords(words: Array<String>, letters: CharArray, score: IntArray): Int {
-        check(score.size == ALPHABET_SIZE)
-        val remainingLetters = IntArray(ALPHABET_SIZE)
-        letters.forEach { ch -> remainingLetters[ch - 'a']++ }
-        val wordsScore = words.map { word -> wordScore(word, score) }.toList()
-        return maxScore(words, 0, wordsScore, remainingLetters)
-    }
+        val letterCount = IntArray(ALPHABET_SIZE) { 0 }
+        letters.map { letter -> letterCount[letter - 'a']++ }
 
-    private fun maxScore(words: Array<String>, wordIndex: Int, wordsScore: List<Int>, remainingLetters: IntArray): Int {
-        if (wordIndex == words.size) return 0
-        val skipWordScore = maxScore(words, wordIndex + 1, wordsScore, remainingLetters)
-        if (!isCollectWord(words[wordIndex], remainingLetters)) return skipWordScore
-        words[wordIndex].forEach { ch -> remainingLetters[ch - 'a']-- }
-        val addWordScore = maxScore(words, wordIndex + 1, wordsScore, remainingLetters) + wordsScore[wordIndex]
-        words[wordIndex].forEach { ch -> remainingLetters[ch - 'a']++ }
-        return max(addWordScore, skipWordScore)
-    }
+        val n = words.size
+        return (1 until (1 shl n)).maxOf { bitmask ->
+            val currentLetterCount = IntArray(ALPHABET_SIZE) { 0 }
+            (0 until n)
+                .filter { bitmask and (1 shl it) != 0 }
+                .map { words[it].forEach { letter -> currentLetterCount[letter - 'a']++ } }
 
-    private fun isCollectWord(word: String, count: IntArray): Boolean {
-        val wordLetterCount = IntArray(ALPHABET_SIZE)
-        word.forEach { ch -> wordLetterCount[ch - 'a']++ }
-        return (0 until ALPHABET_SIZE).all { letterIndex -> wordLetterCount[letterIndex] <= count[letterIndex] }
+            val take = (0 until ALPHABET_SIZE).all { index -> currentLetterCount[index] <= letterCount[index] }
+            if (take) currentLetterCount.indices.sumOf { index -> currentLetterCount[index] * score[index] }
+            else 0
+        }
     }
-
-    private fun wordScore(word: String, score: IntArray) = word.map { ch -> score[ch - 'a'] }.sum()
 
     companion object {
         const val ALPHABET_SIZE: Int = 26
