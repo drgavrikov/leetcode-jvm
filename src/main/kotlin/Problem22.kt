7f9fcc26b91541a2fb0572ce39aa049169b8e291
dr_gavrikov <agavrikov1989@gmail.com>
2024-06-20 20:37:57 +0400

    problem-22

--- src/main/kotlin/Problem22.kt
+++ src/main/kotlin/Problem22.kt
@@ -1,29 +1,43 @@
 /**
  * @author Aleksandr Gavrikov
- * @url https://leetcode.com/problems/generate-parentheses/
+ * @url https://leetcode.com/problems/generate-parentheses/solutions/5342674/generation-of-all-bracket-sequences-based-on-obtaining-the-next-one-without-recursion/
  */
 class Problem22 {
 
     fun generateParenthesis(n: Int): List<String> {
+        val parentheses = CharArray(n * 2) { if (it < n) '(' else ')' }
         val result = mutableListOf<String>()
-        fun brute(brackets: String = "", opened: Int = 0, closed: Int = 0) {
-            if (brackets.length == 2 * n) {
-                result.add(brackets)
-                return
+
+        do {
+            result.add(parentheses.concatToString())
+        } while (nextPermutation(parentheses))
+        return result
+    }
+
+    private fun nextPermutation(parentheses: CharArray): Boolean {
+        val size = parentheses.size / 2
+        var openedCount = size
+        var closedCount = size
+        for (index in parentheses.size - 1 downTo 0) {
+            if (parentheses[index] == ')') closedCount-- else openedCount--
+
+            if (parentheses[index] == '(' && openedCount > closedCount) {
+                parentheses[index] = ')'
+                val toIndex = (index + 1) + (size - openedCount)
+                parentheses.fill('(', index + 1, toIndex)
+                parentheses.fill(')', toIndex)
+                return true
             }
-            if (opened < n) brute("$brackets(", opened + 1, closed)
-            if (opened > closed) brute("$brackets)", opened, closed + 1)
         }
-        brute()
-        return result.sorted()
+        return false
     }
 }
 
 fun main() {
     val problem = Problem22()
 
-    val actual = problem.generateParenthesis(3)
-    check(actual == listOf("((()))", "(()())", "(())()", "()(())", "()()()")) {
-        "Expected ${listOf("((()))", "(()())", "(())()", "()(())", "()()()")}, but got: $actual"
-    }
+    //problem.nextPermutation("(())(())".toCharArray())
+
+    val excepted = listOf("((()))", "(()())", "(())()", "()(())", "()()()")
+    check(problem.generateParenthesis(3) == excepted)
 }
