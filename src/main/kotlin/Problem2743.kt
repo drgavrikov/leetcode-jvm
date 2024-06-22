4d6ac1fa344081dd1ce82dfc1e797244b9e22e70
dr_gavrikov <agavrikov1989@gmail.com>
2024-06-22 23:07:12 +0400

    problem-2743

--- src/main/kotlin/Problem2743.kt
+++ src/main/kotlin/Problem2743.kt
@@ -6,15 +6,12 @@ class Problem2743 {
     fun numberOfSpecialSubstrings(s: String): Int {
         var count = 0
         val frequencies = HashMap<Char, Int>()
-        var countNotUnique = 0
 
         var left = 0
         for (right in s.indices) {
             frequencies[s[right]] = frequencies.getOrDefault(s[right], 0) + 1
-            if (frequencies.getValue(s[right]) == 2) countNotUnique++
-            while (countNotUnique > 0) {
+            while (frequencies.getValue(s[right]) > 1) {
                 frequencies[s[left]] = frequencies.getValue(s[left]) - 1
-                if (frequencies.getValue(s[left]) == 1) countNotUnique--
                 left++
             }
             count += right - left + 1
