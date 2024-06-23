920ff8ce3702611893eadd7cdfea47cac1babf39
dr_gavrikov <agavrikov1989@gmail.com>
2024-06-23 20:23:07 +0400

    problem-1438

--- src/main/kotlin/Problem1438.kt
+++ src/main/kotlin/Problem1438.kt
@@ -1,25 +1,29 @@
-import java.util.*
+import kotlin.collections.ArrayDeque
 import kotlin.math.max
 
 /**
  * @author Aleksandr Gavrikov
- * @url https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/solutions/5357132/sliding-window-and-multiset/
+ * @url https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/solutions/5357132/sliding-window-solution/
  */
 class Problem1438 {
     fun longestSubarray(nums: IntArray, limit: Int): Int {
-        val multiset = TreeMap<Int, Int>()
+        val maxDeque = ArrayDeque<Int>()
+        val minDeque = ArrayDeque<Int>()
         var result = 0
 
         var left = 0
         for (right in nums.indices) {
             val numRight = nums[right]
-            multiset[numRight] = multiset.getOrDefault(numRight, 0) + 1
+            while (maxDeque.isNotEmpty() && maxDeque.last() < numRight) maxDeque.removeLast()
+            maxDeque.addLast(numRight)
 
-            while (multiset.lastKey() - multiset.firstKey() > limit) {
-                val numLeft = nums[left]
-                multiset[numLeft] = multiset.getValue(numLeft) - 1
-                if (multiset.getValue(numLeft) == 0) multiset.remove(numLeft)
-                left++
+            while (minDeque.isNotEmpty() && minDeque.last() > numRight) minDeque.removeLast()
+            minDeque.addLast(numRight)
+
+            while (maxDeque.first() - minDeque.first() > limit) {
+                val numLeft = nums[left++]
+                if (maxDeque.first() == numLeft) maxDeque.removeFirst()
+                if (minDeque.first() == numLeft) minDeque.removeFirst()
             }
             result = max(result, right - left + 1)
         }
@@ -33,4 +37,3 @@ fun main() {
     check(problem.longestSubarray(intArrayOf(8, 2, 4, 7), 4) == 2)
     check(problem.longestSubarray(intArrayOf(10, 1, 2, 4, 7, 2), 5) == 4)
 }
-
