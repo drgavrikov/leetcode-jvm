c0fb676b1dc6445ddd1c64c65449c3c3e6ec59e9
drgavrikov <agavrikov1989@gmail.com>
2024-07-12 19:36:23 +0300

    update kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
+    kotlin("jvm") version "1.7.0"
 }
 
 group = "leetcode"
